fab1b80b80a2585fbb58d887244d2cd626aff69a
Unknown <mihaly.k98@gmail.com>
2018-03-22 14:11:39 +0100

    added to core

---
+++ FunctionTester.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 19 16:29:09 2018
+
+@author: Misa
+"""
+
+import core
+
+core.hisplot([32,32,32,32],[2,2,2,2],[[2000,2003],[2003,2006],[2006,2009],[2009,2012]],200)
+
