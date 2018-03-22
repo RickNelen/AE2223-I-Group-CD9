1dbf4a7c5a29fbf04ef2c673536a89253baf5aef
Unknown <skoewiedoe@gmail.com>
2018-03-22 16:06:30 +0100

    freq+delay

---
+++ lib/Freq-top10.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 22 14:15:10 2018
+
+@author: Laurens
+"""
+
+import core as core
+
+
+k = core.unpickle("../Data.txt")
+k = core.sortata(k,2)
+
+freqlist = []
+i=0
+c=0
+for i in range(1,len(k)):
+    if k[i][5] == k[i-1][5]:
+        c += 1
+    if k[i][5] != k[i-1][5]:
+        j = []
+        j.append(k[i-1][5])
+        j.append(c)
+        freqlist.append(j)
+        c = 0
+        
+freqlist = sorted(freqlist,key=lambda x: x[1] ,reverse=True)
