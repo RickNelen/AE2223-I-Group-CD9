dab207150f8baab5cf8e57328567854dbaa40ac7
Unknown <mihaly.k98@gmail.com>
2018-03-29 14:09:20 +0200

    fun

--- FunctionTester.py
+++ FunctionTester.py
@@ -10,18 +10,18 @@ import lib.core as core
 import time
 
 
-#core.hisplot([32,32,32,32],[2,2,2,2],[[2000,2003],[2003,2006],[2006,2009],[2009,2012]],200)
+core.hisplot([32,32,32,32],[1,2,3,2],[[2000,2003],[2003,2006],[2006,2009],[2009,2012]],600)
 
-data = core.unpickle("./Data.txt")
-
-s = time.time()
-l = core.sortata(data,4)
-e = time.time()
-print e -s
-
-
-s = time.time()
-k = core.findunigue(data, 1,4)
-e = time.time()
-print e -s
+#data = core.unpickle("./Data.txt")
+#
+#s = time.time()
+#l = core.sortata(data,4)
+#e = time.time()
+#print e -s
+#
+#
+#s = time.time()
+#k = core.findunigue(data, 1,4)
+#e = time.time()
+#print e -s
 
