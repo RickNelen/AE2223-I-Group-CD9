c49ba42c9818a880e4a6b6300670cacdee24b054
Laurens <skoewiedoe@gmail.com>
2018-05-24 09:57:25 +0200

    threedgraphupdate

--- lib/core.py
+++ lib/core.py
@@ -313,15 +313,40 @@ def sortata(data,digits):
 
 
 
-
+def getfleetsize(timeframe , interval, k):
+    import numpy as np
+    import datetime
+    
+    fleetsize = []
+    fleetsize1 = []
+    datelist = []
+    for l in np.arange(timeframe[0]*12, timeframe[1]*12+12,interval):   #overall loop for years. Till put this to 27, since we only have 2016 data until feb or so
+        aa = datetosec(int(np.floor(l/12)),int(l%12)+1,1) # lower limit converted to unix
+        ab = datetosec(int(np.floor((l+interval-1)/12)),int((l+interval-1)%12)+1,31) # upper limit in unix
+        t = []
+        
+        datelist.append(datetime.datetime.fromtimestamp(aa).strftime('%d/%m/%Y')) 
+        
+        for j in range(1,len(k)):                                                   #loop for filtering per year 
+            if aa <= k[j][2] < ab:
+                t.append(k[j][0])
+    
+        t = list(set(t))
+        t.sort(key= lambda x:x)
+        fleetsize.append(len(t))
+        fleetsize1.append(t)
+        t = []
+        
+    return fleetsize, fleetsize1
 
 #def getdelaylist(type = 0, timeframe = [1988,2015], interval = 36, k):
 def getdelaylist(timeframe , interval, k): # interval in months, type = 0 means all types
     import numpy as np
     import datetime
     
-    datelist = []
     final = []
+    datelist = []
+
     for l in np.arange(timeframe[0]*12, timeframe[1]*12+12,interval):   #overall loop for years. Till put this to 27, since we only have 2016 data until feb or so
         aa = datetosec(int(np.floor(l/12)),int(l%12)+1,1) # lower limit converted to unix
         ab = datetosec(int(np.floor((l+interval-1)/12)),int((l+interval-1)%12)+1,31) # upper limit in unix
@@ -516,6 +541,121 @@ def makebumpplot(csvname, epsname, title, timelabels, top):  # assumed input csv
     #plt.show()
     
     
+    
+def ThreeDgraph(delay, periodnumber, graph):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from mpl_toolkits import mplot3d
+    
+    for i in range(len(delay)):                                                     #removing all the 0 delays.
+        delay[i] = [x for x in delay[i] if x[2] != 0]
+    output = []
+    
+    for year in range(len(delay)):
+        cmap = []
+        x1 = []
+        x2 = []
+        x3 = []
+        x4 = []
+        x5 = []
+        y1 = []
+        y2 = []
+        y3 = []
+        y4 = []
+        y5 = []
+        z1 = []
+        z2 = []
+        z3 = []
+        z4 = []
+        z5 = []
+        Aircraftgen = [0,1,2,3,4,5,6,7,8,9,10,11,12,14,18] #->blue
+        airframesystem = [20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50] #-> pink
+        structure = [51,52,53,54,55,56,57] #-> orange
+        proprotor = [60,61,62,63,64,65,66,67] #-> red
+        powerplant = [70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85.91,92] #-> black
+        for i in range(len(delay[year])):
+            if int(str(delay[year][i]).strip('[')[:2]) in Aircraftgen:
+                cmap.append('b')
+                x1.append(delay[year][i][1])#/delay[year][i][2])
+                y1.append(delay[year][i][2])
+                z1.append(delay[year][i][3])#/float(delay[year][i][3]+delay[year][i][2]))
+            if int(str(delay[year][i]).strip('[')[:2]) in airframesystem:
+                cmap.append('m')
+                x2.append(delay[year][i][1])#/delay[year][i][2])
+                y2.append(delay[year][i][2])
+                z2.append(delay[year][i][3])#/float(delay[year][i][3]+delay[year][i][2]))
+            if int(str(delay[year][i]).strip('[')[:2]) in structure:
+                cmap.append('g')
+                x3.append(delay[year][i][1])#/delay[year][i][2])
+                y3.append(delay[year][i][2])
+                z3.append(delay[year][i][3])#/float(delay[year][i][3]+delay[year][i][2]))
+            if int(str(delay[year][i]).strip('[')[:2]) in proprotor:
+                cmap.append('r')
+                x4.append(delay[year][i][1])#/delay[year][i][2])
+                y4.append(delay[year][i][2])
+                z4.append(delay[year][i][3])#/float(delay[year][i][3]+delay[year][i][2]))
+            if int(str(delay[year][i]).strip('[')[:2]) in powerplant:
+                cmap.append('k')      
+                x5.append(delay[year][i][1])#/delay[year][i][2])
+                y5.append(delay[year][i][2])
+                z5.append(delay[year][i][3])#/float(delay[year][i][3]+delay[year][i][2]))
+        
+        data1 = (np.asarray(x1),np.asarray(y1),np.asarray(z1))
+        data2 = (np.asarray(x2),np.asarray(y2),np.asarray(z2))
+        data3 = (np.asarray(x3),np.asarray(y3),np.asarray(z3))
+        data4 = (np.asarray(x4),np.asarray(y4),np.asarray(z4))
+        data5 = (np.asarray(x5),np.asarray(y5),np.asarray(z5))
+        data = (data1,data2,data3,data4,data5)
+        colors = ('b', 'm', 'g', 'r', 'k')
+    #    linlsqx = np.asarray(x1 + x2 + x3 + x4 +x5)# + np.asarray(x2) + np.asarray(x3) + np.asarray(x4) + np.asarray(x5)
+    #    linlsqy = np.asarray(y1 + y2 + y3 + y4 +y5)# + np.asarray(y2) + np.asarray(y3) + np.asarray(y4) + np.asarray(y5)
+        groups = ('aircraft general','airframe systems','structure','propellor / rotor','powerplant')
+    #    aaa = np.linalg.lstsq(linlsqx, linlsqy)
+        
+        
+        xtot = x1+x2+x3+x4+x5
+        ytot = y1+y2+y3+y4+y5
+        ztot = z1+z2+z3+z4+z5
+        lstsq = []
+        for i in range(len(xtot)):
+            ab = xtot[i]/ytot[i]
+            lstsq.append(ab)
+    #    avgavgelay = round(float(sum(lstsq))/len(lstsq),2)
+        xttot = np.asarray(xtot)
+        yttot = np.asarray(ytot)
+        zz = np.polyfit(xttot, yttot, 1)
+        zz = list(zz)
+        lstx = [zz[:-1],(max(xtot)*zz[0]+zz[:-1])]
+        lsty = [0,max(xtot)]
+        if year == periodnumber:
+            for jk in range(len(xtot)):
+                temp12 = []
+                temp12.append(xtot[jk])
+                temp12.append(ytot[jk])
+                temp12.append(ztot[jk])
+                output.append(temp12)
+        if graph == True:
+            fig = plt.figure()
+            ax = fig.add_subplot(1, 1, 1, axisbg="1.0")
+            ax = fig.gca(projection='3d')
+            for data, color, group in zip(data, colors, groups):
+                x, y, z = data
+                ax.scatter(x, y, z, alpha=0.65, c=color, s=10, label=group)
+            if year == 7:
+                ax.set_xlim(0,500)
+            
+            
+            ax.plot(lsty,lstx, alpha = 0.65, c = 'black')
+            #ax.set_ylim(0,30)
+            #ax.set_zlim(0,30)
+            ax.set_xlabel('total delay')
+            ax.set_ylabel('delay frequency')
+            ax.set_zlabel('cancellation frequency')#cancellation frequency
+            plt.title('3D scatterplot period '+ str(year+1))
+            plt.legend(loc=2)
+            plt.show()
+    return output
+        
     
     
     
