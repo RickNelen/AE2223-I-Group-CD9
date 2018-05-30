hm, I had to redo some changes I had already made. Please everyone, be more careful when resolving conflicts between versions and actually look at code! Got nice level plots for the covariance, fixed the delay variance script according to the above issue, got linear fitting to work (last function in numericals.py)

Variance.py
@@ -12,18 +12,26 @@ import pickle
 import matplotlib.pyplot as plt
 import datetime
 import time
+import copy
 
-
-# importing the final top 10 lists
-from DelayTop10 import getdelaylist
-# arguments are the atalevel, year range and interval in months. More are possible, check the function
-final_delay = getdelaylist(2, timeframe = [1988,2015], interval = 28*12) # 28*12 are the months included on the total year range
+actype = 1
+atalevel = 2
+interval = 36
 
 with open("./Data.txt", "rb") as fp:   # Unpickling list all types
    b = pickle.load(fp)
 
+a = copy.deepcopy(b)
+a = core.getbyType(a, actype)
+a = core.sortata(a, atalevel)
 
-delaycutoff = 15 # min time to actually consider something a delay
+# importing the final top 10 lists
+# from DelayTop10 import getdelaylist
+# arguments are the atalevel, year range and interval in months. More are possible, check the function
+final_delay, datelist = core.getdelaylist(timeframe = [1988,2015], interval = 28*12, k = a) # 28*12 are the months included on the total year range
+
+
+delaycutoff = 15 # min aatime to actually consider something a delay
 #binsize = 5 # unused at the moment, was used for debugging
 
 #actype = 1 # all this was used for debugging
@@ -35,30 +43,31 @@ delaycutoff = 15 # min time to actually consider something a delay
 #c = core.getbyCancelled(c, 0)
 
 
-
-
 ### Section on fitting stuff to the delay time distributions
 j = 0
-for yeardata in final_delay[0]:
+for yeardata in final_delay:
     atas = []
     for item in yeardata:
         atas.append(item[0])
     
-    if len(final_delay[1]) == 1: # if only one entry in the delay ranking, we assume the entire time range was used.
+    if len(final_delay) == 1: # if only one entry in the delay ranking, we assume the entire time range was used.
         ll = int(time.mktime(datetime.datetime.strptime('01/01/1988', "%d/%m/%Y").timetuple()))  # converted to unixtime
         ul = int(time.mktime(datetime.datetime.strptime('31/12/2015', "%d/%m/%Y").timetuple()))  # converted to unixtime
+        interv = 28*12
     else:
         # lower limit of time interval for which variance needs to be computed. This is stored in here by the getdelaylist function.
         # See DelayTop10.py for details
-        ll = int(time.mktime(datetime.datetime.strptime(final_delay[1][j], "%d/%m/%Y").timetuple()))  # converted to unixtime
+        ll = int(time.mktime(datetime.datetime.strptime(datelist[j], "%d/%m/%Y").timetuple()))  # converted to unixtime
         # reverse engineer interval from first two entries
-        interv = int(time.mktime(datetime.datetime.strptime(final_delay[1][1], "%d/%m/%Y").timetuple())) - int(time.mktime(datetime.datetime.strptime(final_delay[1][0], "%d/%m/%Y").timetuple()))
+        interv = int(time.mktime(datetime.datetime.strptime(datelist[1], "%d/%m/%Y").timetuple())) - int(time.mktime(datetime.datetime.strptime(datelist[0], "%d/%m/%Y").timetuple()))
         ul = ll + interv
         
     c = core.getbyTimestamp(b,[ll, ul])
     
     k = 0
     for ata in atas:
+        
+        
         dataset = core.getbyATA(c, ata)
         
         if len(dataset) < 2: # check if emtpy, shouldnt happen for the top10 stuff
@@ -77,15 +86,16 @@ for yeardata in final_delay[0]:
         #bins = range(delays.min(), delays.max(), binsize) # used for debugging
         hist = [delays, np.histogram(delays, range=[delaycutoff,delays.max()], bins='fd', density=True)]
     
-        if True: #ata == 324 and j == 20:
-            (fitbeta, mean, var) = numericals.fitweibull(hist, 1) # also plot it, that's what the 1 is for
+        if False: #ata == 324 and j == 20:
+            (fitbeta, mean, var, cov) = numericals.fitweibull(hist, 1, "Ata %d -- " % ata + datetime.datetime.fromtimestamp(ll).strftime('%Y-%m') + " to " + datetime.datetime.fromtimestamp(ul).strftime('%Y-%m') + " -- Interval %d Months" % interv) # also plot it, that's what the 1 is for
         else:
-            (fitbeta, mean, var) = numericals.fitweibull(hist, 0)
+            (fitbeta, mean, var, cov) = numericals.fitweibull(hist, 0)
         
         
-        final_delay[0][j][k].append(fitbeta[0]) # don't remember why they are 1 component lists in this case... the [0] makes it a little neater
-        final_delay[0][j][k].append(mean[0])
-        final_delay[0][j][k].append(var[0])
+        final_delay[j][k].append(fitbeta[0]) # don't remember why they are 1 component lists in this case... the [0] makes it a little neater
+        final_delay[j][k].append(mean[0])
+        final_delay[j][k].append(var[0])
+        final_delay[j][k].append(cov[0])
         
         k += 1
         
@@ -97,24 +107,27 @@ import csv
 
 intl = []
 newl = []
-for yeardata in final_delay[0]:
+w = 0
+for yeardata in final_delay:
     for k in range(10):
         #print w, k
         intl.append(yeardata[k][0])   # ata
         intl.append(k+1) # rank
         intl.append(yeardata[k][1])    # tot delay
-        intl.append(final_delay[1][w]) # data
-        intl.append(yeardata[k][2][0])   # weibk
-        intl.append(yeardata[k][2][1])   # weibLamb
-        intl.append(yeardata[k][3])   # DelMean
-        intl.append(np.sqrt(yeardata[k][4]))   # DelStdDev
+        intl.append(datelist[w]) # date
+        intl.append(yeardata[k][6][0])   # weibk
+        intl.append(yeardata[k][6][1])   # weibLamb
+        intl.append(yeardata[k][7])   # DelMean
+        intl.append(np.sqrt(yeardata[k][8]))   # DelStdDev
+        intl.append(yeardata[k][9])   # Parameter covariance matrix off-diagonal
         
         newl.append(intl)
         intl = []
+    w = w+1
         
 with open("DelayVariancesTotalTop10.csv", 'wb') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-    wr.writerow(['ATA', 'Rank', 'TotDelay', 'Date', 'Weibk', 'WeibLamb', 'DelMean', 'DelStdDev'])
+    wr.writerow(['ATA', 'Rank', 'TotDelay', 'Date', 'Weibk', 'WeibLamb', 'DelMean', 'DelStdDev', 'rho_xy'])
     for row in newl:
         wr.writerow(row)
 

covariance.py
@@ -13,16 +13,20 @@ from mpl_toolkits.mplot3d import Axes3D
 k = core.unpickle("./Data.txt")                                                 #getting the data ready
 k = core.sortata(k, 4)
 delay, date = core.getdelaylist([1988,2015],36,k)                               #getting the data ready till here
-output = core.ThreeDgraph(delay, 1, 0)
+output = core.ThreeDgraph(delay, 2, 0)
 
 columns = [[], [], []]
 for line in output:
-    if line[1] > 100: # can be used for filtering
+    if line[1] > 0: # can be used for filtering
         columns[0].append(line[0])
         columns[1].append(line[1])
         columns[2].append(line[2])
     
 data = np.array(columns)
+data = data.astype(float)
+data[0] = data[0] / float(max(data[0]))
+data[1] = data[1] / float(max(data[1]))
+data[2] = data[2] / float(max(data[2]))
 
 covmat = np.cov(data)   # get covariance matrix
 eigvals, eigvecs = np.linalg.eig(covmat) # get eigenstuff
@@ -126,9 +130,34 @@ def plane_plane_intersect(N1,A1,N2,A2):
     
     return P,N,check
 
+
+# plotting
+
+# level lines
+fig2 = plt.figure()
+ax = fig2.add_subplot(111)
+for z in np.linspace(0,max(data[2]),5):
+    P2, N2, check2 = plane_plane_intersect(np.array([0., 0., 1.]), np.array([0., 0., float(z)]), np.cross(eigvecs[:,maxevs[0]],eigvecs[:,maxevs[1]]), mpoint)
+    a = N2[1]/N2[0]
+    b = P2[1] - P2[0]* N2[1]/N2[0]
+    x = np.linspace(0,max(data[0]), 101)
+    y = a*x+b
+    ax.plot(x,y, label="Cancellation frequency %.2f" % z)
+    #print ("Equation of the line: y = %f x + %f" % (N2[1]/N2[0], P2[1] - P2[0]* N2[1]/N2[0]))
+
+x = data[0]
+y = data[1]
+z = data[2]
+ax.scatter(x,y,z/float(max(z)) * 50, label='Data, size proportional to cancellations')
+plt.title('Level curves of the dimensionality reduction plane')
+plt.xlabel('Total delay time')
+plt.ylabel('Delay frequency')
+plt.ylim((0, max(data[1])))
+ax.legend()
+
+
+# 3 dim
 P,N,check = plane_plane_intersect(np.array([0., 0., 1.]), np.array([0., 0., 0.]), np.cross(eigvecs[:,maxevs[0]],eigvecs[:,maxevs[1]]), mpoint)
-
-
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 #ax.set_aspect('equal')

lib/numericals.py
@@ -264,7 +264,7 @@ def lstsquares(model, n, beta, data, h=1e-4): # obsolete, cause I am now using a
 
 
 
-def fitweibull(histo, plot = 0):
+def fitweibull(histo, plot = 0, plottitle = ""):
     # takes _one_ normalized histogram from np.histogram and fits a weibull to it using init guess k=1.5, lambda = 20.
     # now uses scipy builtin function for the fitting
     
@@ -325,7 +325,8 @@ def fitweibull(histo, plot = 0):
             yfit = weibull(xplot,fb[0], fb[1])
             
             plt.close()
-            plt.title("ATA Component 324 -- Year 2008 -- Stdev: %.1f min -- parcor %.3f" % (np.sqrt(v), cov[0,1]) )
+            #plt.title("ATA Component 324 -- Year 2008 -- Stdev: %.1f min -- parcor %.3f" % (np.sqrt(v), cov[0,1]) )
+            plt.title(plottitle + " -- Stdev: %.1f min -- parcor %.3f" % (np.sqrt(v), cov[0,1]) )
             plt.xlabel("Delay Time [min]")
             plt.ylabel("Density")
             plt.plot(xplot,yfit)
@@ -337,8 +338,36 @@ def fitweibull(histo, plot = 0):
         i += 1
         
     
-    return (fitbeta, mean, var)
+    return (fitbeta, mean, var, cov[0,1])
     
+    
+def polynomial_fit(x,y,degree, plot): # x and y are separate lists of values. Degree is an integer. Plot is for debugging, either 0 or 1
+    # this is a quick shortcut for getting the coefficients of a polinomial of "degree" degrees.
+    # in any case, the covariance matrix is output using some bayesian method (sound good at least)
+    # also I include the pearson r which indiates the linear correlation (regardless of offsets btw)
+    # of the dataset on a scale from -1 (negatively correlated) over 0 (not linearly correlated) to 1 (positively correlated)
+        
+    from numpy import polyfit, linspace
+    from scipy.stats import pearsonr
+    
+    # do the fitting using linear least squares with the Vdm matrix
+    p, V = polyfit(x,y,degree, cov = True)
+    
+    # get the (linear correlation) using the pearson correlation coefficient
+    r = pearsonr(x,y)
+    
+    if plot:
+        from matplotlib import pyplot as plt
+        xcomp = linspace(min(x), max(x), 101)
+        ycomp = 0
+        for i in range(degree+1): # getting the linear solution using a sort-of dot product with the coefficients vector p and all the x points (to the power of the degree the current coefficient p[i] corresponds to)
+            ycomp += xcomp**i * p[i]
+            
+        plt.plot(xcomp,ycomp)
+        plt.scatter(x,y)
+        plt.title(str(p))
+    
+    return p, V, r
     
     
     